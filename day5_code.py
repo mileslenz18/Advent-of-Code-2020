ba259240ebe8ef447efca822341a6fd302abc0c9
Miles Lenz <lenz.miles18@gmail.com>
2021-06-23 19:20:55 +0200

    added solution for day 5 (part one + two)

---
+++ day5_code.py
@@ -0,0 +1,50 @@
+from helper_functions import getFileContet
+
+
+def part_one(seatIDs):
+    """Find highest seat ID on a boarding pass."""
+
+    maxID = max(seatIDs)
+
+    return maxID
+
+
+def part_two(seatIDs):
+    """Find your own seat ID."""
+
+    seatIDs.sort()
+
+    for i in range(len(seatIDs[:-1])):
+        if seatIDs[i+1] - seatIDs[i] > 1:
+            mySeat = seatIDs[i] + 1
+
+    return mySeat
+
+
+def main():
+    content = getFileContet('day5_input.txt')
+
+    seatIDs = []
+
+    for seat in content:
+        rowNo, colNo = '', ''
+        row, col = seat[:7], seat[7:]
+
+        for char in row:
+            rowNo += '0' if char == 'F' else '1'
+
+        for char in col:
+            colNo += '0' if char == 'L' else '1'
+
+        seatID = int(rowNo, 2) * 8 + int(colNo, 2)
+        seatIDs.append(seatID)
+
+    res1 = part_one(seatIDs)
+    print(f'Part One: {res1}')
+
+    res2 = part_two(seatIDs)
+    print(f'Part Two: {res2}')
+
+
+if __name__ == '__main__':
+    main()
